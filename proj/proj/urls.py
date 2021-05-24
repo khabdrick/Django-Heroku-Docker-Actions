3213e102fb6afe5b4a3504d54d487c2d7ced2bdd
Muhammed Ali <muhamzyali@gmail.com>
2021-05-24 11:50:21 +0100

    url

--- proj/proj/urls.py
+++ proj/proj/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ..app.views import homePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", homePageView.as_view, name= "how"),
 ]
