29f7fa5c4aadf00c0ffd8ea16d12b0b7aec1bd59
Muhammed Ali <muhamzyali@gmail.com>
2021-05-24 12:07:15 +0100

    new app

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zx@pwxrcdvj-*#7pc_$lbt2xjjhc-fkmh6=9anl-htgs-z+5*n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '0.0.0.0' ]
 
 
 # Application definition
--- proj/proj/urls.py
+++ proj/proj/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ..app.views import homePageView
+from app.views import homePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", homePageView.as_view, name= "how"),
+    path("", homePageView, name= "home"),
 ]
