ac414893d6cb80d55b77eaadb7a81d9e9b2d0215
Muhammed Ali <muhamzyali@gmail.com>
2021-06-10 10:22:03 +0100

    updated app/views.py

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def homePageView(request):
-    return HttpResponse('Hello, World!')
+    return HttpResponse('Hello, World!. Just added Continuous Deployment')
